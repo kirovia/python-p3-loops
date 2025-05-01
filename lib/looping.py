d6cfdb36acff2ad1190754f8659955c36463d66d
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-05-01 16:12:03 -0400

    Passes all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        if i == 0:
+            print('Happy New Year!')
+        else:
+            print(i)
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int * int for int in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+            i += 1
+        elif i % 3 == 0:
+            print('Fizz')
+            i += 1
+        elif i % 5 == 0:
+            print('Buzz')
+            i += 1
+        else:
+            print(i)
+            i += 1
